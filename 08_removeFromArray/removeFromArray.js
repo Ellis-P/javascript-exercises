b704035e386d45e70753e55fa544a9e6ecdcc11f
Ellis <222089829+Ellis-P@users.noreply.github.com>
2025-08-03 22:26:52 +0200

    Complte 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...items) {
+  const arrayCopy = array.slice();
+
+  for (const element of array) {
+    if (items.includes(element)) {
+      let index = arrayCopy.indexOf(element);
+      arrayCopy.splice(index, 1);
+    }
+  }
+  return arrayCopy;
 };
 
 // Do not edit below this line
