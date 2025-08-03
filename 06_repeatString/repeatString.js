b702a89a1266c477086b64761a9f11c8fffb7679
Ellis <222089829+Ellis-P@users.noreply.github.com>
2025-08-03 18:13:43 +0200

    Complete 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+  let count = 0;
+  let repeatedString = "";
 
+  if (num < 0) {
+    return "ERROR";
+  }
+
+  while (count < num) {
+    repeatedString += string;
+    count++;
+  }
+  return repeatedString
 };
-
 // Do not edit below this line
 module.exports = repeatString;
