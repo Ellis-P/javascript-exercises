b5b5acd31eff5a91778e39092c7bfce467fefbf9
Ellis <222089829+Ellis-P@users.noreply.github.com>
2025-08-04 10:15:23 +0200

    Complete 11_tempConversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+  const cTemp = (fTemp - 32) * (5/9);
+  return Math.round(cTemp * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+  const fTemp = ((cTemp * (9/5)) + 32);
+  return Math.round(fTemp * 10) / 10;
 };
+
+// C to F = ((cTemp * (9/5)) + 32)
+// F to C = (fTemp - 32) * (5/9)
 
 // Do not edit below this line
 module.exports = {
